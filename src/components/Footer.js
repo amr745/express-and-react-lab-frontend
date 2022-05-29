ef81fa2331cc1ce974dc9166c53b9532d99a0bf7
Ashley Richard <ashleymrichard@gmail.com>
2022-05-29 01:13:54 -0700

    some css styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,21 @@
+// import { useState, useEffect } from "react";
+
 function Footer(props) {
-    return <h1>Footer</h1>;
+
+  const social= () => {
+    window.location.href="https://www.linkedin.com/in/ashleymrichard1";
   }
-  
-  export default Footer;
+
+  const git= () => {
+    window.location.href = "https://github.com/amr745/express-and-react-lab-frontend";
+  }
+  return (
+  <footer>
+    <p>Created by Ashley M. Richard</p>
+    <img className="social-link" src="https://imgur.com/8VOL4YY.png" alt="Linkedin Link" onClick={social}/>
+    <img className="social-link" src="https://imgur.com/YTC2jT5.png"alt="Github Link" onClick={git}/>
+  </footer>
+  )
+}
+
+export default Footer;
